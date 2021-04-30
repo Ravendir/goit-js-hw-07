fc1f5b9047822fdef8e174461a7eaa740bfc5d26
Valerian Rykov <Valerra2@gmail.com>
2021-04-30 19:46:57 +0300

    ошибка в 5ой

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 let input = document.querySelector("#name-input");
 let nameOutput = document.querySelector("#name-output");
 
-input.onInput = function () {
+input.oninput = function () {
   if (input.value === "") {
     nameOutput.innerHTML = "незнакомец";
   } else {
